9b5c6c509497b7274cd1a83dc61e9d0f72b892e7
Przemysław Gwóźdź <przemyslaw.gwozdz@ibcs.pl>
2023-07-13 12:46:40 +0200

    Change decorator

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -5,6 +5,7 @@ import { SignInDto } from 'src/shared/dtos/auth/sign-in.dto';
 import { JwtAuthGuard } from 'src/shared/guards/jwt-auth.guard';
 import { RefreshTokenGuard } from 'src/shared/guards/refresh-token.guard';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { CurrentUser } from "src/shared/decorators/current-user/current-user.decorator";
 
 @ApiTags('Authentication')
 @Controller('auth')
